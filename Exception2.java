23a960f479601a7073359364857ec1af34aa7056
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-03-19 13:53:00 +0530

    Update Exception2.java

--- Exception2.java
+++ Exception2.java
@@ -3,7 +3,7 @@
 public class Exception2 {
     public static void main(String[] args) {
         try {
-            String s = null;
+            String s = null;  // Empty String
             System.out.println(s.length());
         } catch (Exception e) {
             System.out.println("pahele s me kya hai wo toh bta re");
