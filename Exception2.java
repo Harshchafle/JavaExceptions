72016edbac259885ce2e9995541a6acfb26992e7
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-03-19 13:42:30 +0530

    Add files via upload

---
+++ Exception2.java
@@ -0,0 +1,12 @@
+// Null Pointer Exception
+
+public class Exception2 {
+    public static void main(String[] args) {
+        try {
+            String s = null;
+            System.out.println(s.length());
+        } catch (Exception e) {
+            System.out.println("pahele s me kya hai wo toh bta re");
+        }
+    }
+}
