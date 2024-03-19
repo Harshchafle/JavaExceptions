63055163407bbe6748584f6a2d2590df6ad6f2fb
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-03-19 13:51:38 +0530

    Update Exception1.java

--- Exception1.java
+++ Exception1.java
@@ -4,13 +4,13 @@ public class Exception1 {
     public static void main(String args[]) {
        
         try {
-            int a = 5/0;
+            int a = 5/0;  // This Statement causes the Arithematic Exception which is subclass of Exception Class
             System.out.println("a = "+a);
            
-        } catch (Exception e) {
+        } catch (Exception e) {   // Handling the Exception With the catch Block ( we can use perfect catch like ArithematicException e)
 
             System.out.println("5 ko zero se divide kr rha re tu");
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 }
